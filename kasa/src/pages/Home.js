48c6a7ce7d40daccaff656ef613b53d717a3dd6e
pivix <stevynguyen@gmail.com>
2024-01-10 08:39:41 +0100

    +banner page home

--- kasa/src/pages/Home.js
+++ kasa/src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import img1 from '@/img/source1.svg'
 
 const Home = () => {
     return (
-        <div className='home'>
-            Home works !
-        </div>
+        <section className='hbanner'>
+            <div className='banner'>
+                <img src={img1} alt='Une montagne en bord de mer' className='banner__img1' />
+                <div className='banner__text'>
+                    <p>Chez vous, partout et ailleurs</p>
+                </div>
+            </div>
+        </section>
     );
 };
 
