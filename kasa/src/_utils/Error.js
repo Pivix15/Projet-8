8e85365bc24e3b67ad45678534034c69095af1c5
pivix <stevynguyen@gmail.com>
2024-01-10 11:19:30 +0100

    +page Error 404 | Correction du footer

--- kasa/src/_utils/Error.js
+++ kasa/src/_utils/Error.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div className='error'>
-            Erreur 404 !
-        </div>
+        <section className='error'>
+            <p>404</p>
+            <div>
+                <p>Oups! La page qui vous demandez n'existe pas.</p>
+            </div>
+            <div>
+                <Link to="/home">Retourner sur la page d'accueil</Link>
+            </div>
+        </section>
     );
 };
 
