92ea159cfcbaeaf6bbbf34aa10589a3a11ffb2a1
pivix <stevynguyen@gmail.com>
2024-01-11 14:20:27 +0100

    +FAQ page Propos

--- kasa/src/pages/APropos.js
+++ kasa/src/pages/APropos.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import img2 from '@/img/source2.svg'
+import React, { useState } from 'react';
+import img2 from '@/img/source2.svg';
 
-const APropos = () => {
+const FaqItem = ({ question, Reponse }) => {
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleActive = () => {
+        setIsActive(!isActive);
+    };
+
     return (
-        <section className='pbanner'>
-            <div className='banner'>
-                <img src={img2} alt='Des montagne enneigés' className='banner__img2' />
+        <div className={`faq ${isActive ? 'active' : ''}`}>
+            <div className='question'>
+                <h3>{question}</h3>
+                <svg onClick={toggleActive} xmlns="http://www.w3.org/2000/svg" height="32" width="32" viewBox="0 0 512 512"><path fill="#FFFF" d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" /></svg>
             </div>
-        </section>
+            <div className='reponse'>
+                <p>{Reponse}</p>
+            </div>
+        </div>
     );
 };
 
+const APropos = () => {
+    return (
+        <div>
+            <section className='pbanner'>
+                <div className='banner'>
+                    <img src={img2} alt='Des montagne enneigés' className='banner__img2' />
+                </div>
+            </section>
+            <FaqItem question="Flabilité" Reponse="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." />
+            <FaqItem question="Respect" Reponse="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+            <FaqItem question="Service" Reponse="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+            <FaqItem question="Sécurité" Reponse="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'aulocataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
+        </div>
+    );
+};
 export default APropos;
