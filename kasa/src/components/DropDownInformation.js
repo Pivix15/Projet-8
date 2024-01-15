27b3766f7d13b0e619fdfaf8c37458a9a6dfdcb4
pivix <stevynguyen@gmail.com>
2024-01-15 12:22:12 +0100

    DropDown dans un .js + modif du style en conséquence

--- kasa/src/components/DropDownInformation.js
+++ kasa/src/components/DropDownInformation.js
@@ -8,12 +8,12 @@ const DropDownInformations = ({ question, reponse }) => {
     };
 
     return (
-        <div className={`l--faq ${isActive ? 'active' : ''}`}>
-            <div className='l-question'>
+        <div className={`faq ${isActive ? 'active' : ''}`}>
+            <div className='question'>
                 <h3>{question}</h3>
                 <svg onClick={toggleActive} xmlns="http://www.w3.org/2000/svg" height="32" width="32" viewBox="0 0 512 512"><path fill="#FFFF" d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" /></svg>
             </div>
-            <div className='l-reponse'>
+            <div className='reponse'>
                 <p>{reponse}</p>
             </div>
         </div>
--- kasa/src/pages/APropos.js
+++ kasa/src/pages/APropos.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import img2 from '@/img/source2.svg';
+import DropDownInformations from '@/components/DropDownInformation'
 
-const FaqItem = ({ question, reponse }) => {
+/* const FaqItem = ({ question, reponse }) => {
     const [isActive, setIsActive] = useState(false);
 
     const toggleActive = () => {
@@ -19,7 +20,7 @@ const FaqItem = ({ question, reponse }) => {
             </div>
         </div>
     );
-};
+}; */
 
 const APropos = () => {
     return (
@@ -29,10 +30,10 @@ const APropos = () => {
                     <img src={img2} alt='Des montagne enneigés' className='banner__img2' />
                 </div>
             </section>
-            <FaqItem question="Flabilité" reponse="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." />
-            <FaqItem question="Respect" reponse="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
-            <FaqItem question="Service" reponse="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
-            <FaqItem question="Sécurité" reponse="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'aulocataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
+            <DropDownInformations question="Flabilité" reponse="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." />
+            <DropDownInformations question="Respect" reponse="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+            <DropDownInformations question="Service" reponse="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+            <DropDownInformations question="Sécurité" reponse="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'aulocataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
         </div>
     );
 };
